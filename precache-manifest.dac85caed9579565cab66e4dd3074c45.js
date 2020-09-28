4cf1868c42ab68d00fe107bb5281ac060948e7e5
realize1103 <realize1103@gmail.com>
2020-09-28 17:59:34 +0200

    Updates

---
+++ precache-manifest.dac85caed9579565cab66e4dd3074c45.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e84edba37248e801d70c4e9673eb01e6",
+    "url": "/react-movie_app/index.html"
+  },
+  {
+    "revision": "4932359337aee42dbc1b",
+    "url": "/react-movie_app/static/css/main.e083b577.chunk.css"
+  },
+  {
+    "revision": "001e866db981de6e58f8",
+    "url": "/react-movie_app/static/js/2.a42ba528.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-movie_app/static/js/2.a42ba528.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4932359337aee42dbc1b",
+    "url": "/react-movie_app/static/js/main.e6482bcd.chunk.js"
+  },
+  {
+    "revision": "7638cba589bd8181740d",
+    "url": "/react-movie_app/static/js/runtime-main.8d5c9d53.js"
+  }
+]);

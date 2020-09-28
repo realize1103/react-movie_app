57e9c21a53ab2387669d0221696363402a4bff66
realize1103 <realize1103@gmail.com>
2020-09-28 17:52:50 +0200

    Done!

--- src/App.js
+++ src/App.js
@@ -30,20 +30,20 @@ class App extends React.Component {
             <span className="loader__text">Loading...</span>
           </div>
         ) : (
-            <div className="movies">
-              {movies.map(movie => (
-                <Movie
-                  key={movie.id}
-                  id={movie.id}
-                  year={movie.year}
-                  title={movie.title}
-                  summary={movie.summary}
-                  poster={movie.medium_cover_image}
-                  genres={movie.genres}
-                />
-              ))}
-            </div>
-          )}
+          <div className="movies">
+            {movies.map(movie => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+                genres={movie.genres}
+              />
+            ))}
+          </div>
+        )}
       </section>
     );
   }
